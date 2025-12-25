6e2a14754d9ff19c3f19e84c30e7ff84ce8c38d9
Greg Dunlap <gdd@heyrocker.com>
2025-12-24 16:25:06 -0800

    Filling out some test player data

--- assets/data/season_16.js
+++ assets/data/season_16.js
@@ -23,5 +23,86 @@ export const playersData = [
     name: "Amanda Kimmel",
     birthday: "August 3, 1984",
     img: "../assets/images/amanda-kimmel.jpg"
-  }
+  },
+  {
+    name: "Cirie Fields",
+    birthday: "August 3, 1984",
+    img: "../assets/images/amanda-kimmel.jpg"
+  },
+  {
+    name: "Natalie Bolton",
+    birthday: "August 3, 1984",
+    img: "../assets/images/amanda-kimmel.jpg"
+  },
+  {
+    name: "Erik Reichenbach",
+    birthday: "August 3, 1984",
+    img: "../assets/images/amanda-kimmel.jpg"
+  },
+  {
+    name: "Alexis Jones",
+    birthday: "August 3, 1984",
+    img: "../assets/images/amanda-kimmel.jpg"
+  },
+  {
+    name: "James Clement",
+    birthday: "August 3, 1984",
+    img: "../assets/images/amanda-kimmel.jpg"
+  },
+  {
+    name: "Jason Siska",
+    birthday: "August 3, 1984",
+    img: "../assets/images/amanda-kimmel.jpg"
+  },
+  {
+    name: "Ozzy Lusth",
+    birthday: "August 3, 1984",
+    img: "../assets/images/amanda-kimmel.jpg"
+  },
+  {
+    name: "Eliza Orlins",
+    birthday: "August 3, 1984",
+    img: "../assets/images/amanda-kimmel.jpg"
+  },
+  {
+    name: "Ami Cusack",
+    birthday: "August 3, 1984",
+    img: "../assets/images/amanda-kimmel.jpg"
+  },
+  {
+    name: "Tracy Hughes-Wolf",
+    birthday: "August 3, 1984",
+    img: "../assets/images/amanda-kimmel.jpg"
+  },
+  {
+    name: "Kathy Sleckman",
+    birthday: "August 3, 1984",
+    img: "../assets/images/amanda-kimmel.jpg"
+  },
+  {
+    name: "Chet Welch",
+    birthday: "August 3, 1984",
+    img: "../assets/images/amanda-kimmel.jpg"
+  },
+  {
+    name: "Jonathan Penner",
+    birthday: "August 3, 1984",
+    img: "../assets/images/amanda-kimmel.jpg"
+  },
+  {
+    name: "Joel Anderson",
+    birthday: "August 3, 1984",
+    img: "../assets/images/amanda-kimmel.jpg"
+  },
+  {
+    name: "Mikey Bortone",
+    birthday: "August 3, 1984",
+    img: "../assets/images/amanda-kimmel.jpg"
+  },
+  {
+    name: "Yau-Man Chan",
+    birthday: "August 3, 1984",
+    img: "../assets/images/amanda-kimmel.jpg"
+  },
+
 ]
