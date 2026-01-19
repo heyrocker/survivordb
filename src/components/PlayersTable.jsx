acfc462f1ab81160cb810eac8802c7fb889c86f3
Greg Dunlap <gdd@heyrocker.com>
2026-01-19 15:58:28 -0800

    Added React Icons, implemented icons for booth method on player table

--- src/components/PlayersTable.jsx
+++ src/components/PlayersTable.jsx
@@ -1,3 +1,8 @@
+import { MdOutlineHowToVote } from "react-icons/md";
+import { PiCrown, PiFlagLight } from "react-icons/pi";
+import { FaTruckMedical, FaBan } from "react-icons/fa6";
+import { GiDuel } from "react-icons/gi";
+
 export default function PlayersTable(props) {
   const playerTableEntries = props.playerInfo.map((player) => {
     
@@ -17,7 +22,7 @@ export default function PlayersTable(props) {
         <div className='players-row players-header list-row list-header'>
           <p>Finish</p>
           <p>Name</p>
-          <p>Boot Method</p>
+          <p></p>
         </div>
         {playerTableEntries}
       </div>
@@ -29,8 +34,40 @@ function PlayerTeaser(props) {
   return (
     <div className='players-row list-row'>
       <p>{props.player.finishPosition == 1 ? "Winner" : props.player.finishPosition}</p>
-      <p>{props.player.player.name}</p>
-      <p>{props.player.bootMethod}</p>
+      <p>{props.player.player.name} </p>
+      <p>{ getPlayerBootIcon(props.player.bootMethod, props.player.finishPosition) }</p>
     </div>
   )
+}
+
+function getPlayerBootIcon(bootMethod, finishPosition) {
+  let icon
+  
+  if (finishPosition == 1) {
+    return <a title="Winner"><PiCrown /></a>
+  }
+  
+  switch (bootMethod) {
+    case "Voted Out":
+      icon = <MdOutlineHowToVote />
+      break
+
+    case "Medically Evacuated":
+      icon = <FaTruckMedical />
+      break
+
+    case "Quit":
+    case "Raised Flag on Edge of Extinction":
+      icon = <PiFlagLight />
+      break
+
+    case "Ejected":
+      icon = <FaBan />
+      break
+
+    case "Lost Duel on Redemption Island":
+      icon = <GiDuel />
+  }
+
+  return <a title={bootMethod}>{icon}</a>
 }
