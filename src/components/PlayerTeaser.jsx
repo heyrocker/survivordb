cced6f5ff3c91fdbf04a3fa3ab73855aac48417b
Greg Dunlap <gdd@heyrocker.com>
2026-01-19 11:40:15 -0800

    Refactored table row components to be included in table component code file just to keep everything together

--- src/components/PlayerTeaser.jsx
+++
@@ -1,9 +0,0 @@
-export default function PlayerTeaser(props) {
-  return (
-    <div className='players-row list-row'>
-      <p>{props.player.player.name}</p>
-      <p>{props.player.player.nickname}</p>
-      <p>{props.player.bootMethod}</p>
-    </div>
-  )
-}
