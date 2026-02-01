d56a654650df62c45b2aaaad3d195bc3ff8eb82c
Greg Dunlap <gdd@heyrocker.com>
2026-02-01 12:47:20 -0800

    Nextifying font handling

--- app/layout.js
+++ app/layout.js
@@ -1,12 +1,31 @@
 import "./globals.css";
+import { Outfit, Bebas_Neue } from "next/font/google"
+import Survivant from 'next/font/local'
+
+const survivant = Survivant({
+  src: '../assets/fonts/survivant.ttf',
+  variable: '--font-survivant'
+})
+
+const outfit = Outfit({
+  subsets: ['latin'],
+  display: "swap",
+  variable: '--font-outfit'
+})
+
+const bebas_neue = Bebas_Neue({
+  subsets: ['latin'],
+  weight: '400',
+  display: "swap",
+  variable: '--font-bebas-neue'
+})
 
 export default function RootLayout({ children }) {
   return (
     <html>
       <head>
-        <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Outfit:wght@300;400;500;600;700&display=swap" rel="stylesheet" />
       </head>
-      <body>
+      <body className={[outfit.variable, bebas_neue.variable, survivant.variable].join(' ')}>
         <header>
           <nav>
             <span className="title-text">Survivor<span className="title-highlight">DB</span></span>
