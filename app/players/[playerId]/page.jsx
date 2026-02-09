26d672d41d36d8bda55148bd0e61ba4850191c4b
Greg Dunlap <gdd@heyrocker.com>
2026-02-08 16:17:55 -0800

    Building out player detail page

--- app/players/[playerId]/page.jsx
+++ app/players/[playerId]/page.jsx
@@ -8,8 +8,21 @@ export default async function PlayerDetailPage({ params }) {
   const contentful_access_key = process.env.NEXT_PUBLIC_CONTENTFUL_ACCESS_KEY
   const data = await getContentfulData(getPlayerDetailPageQuery(playerId))
   const playerInfo = data.data.survivorPlayerCollection.items[0]
+  const options = { year: 'numeric', month: 'long', day: 'numeric' }; 
+  const birthday = new Date(playerInfo.birthday).toLocaleDateString("en-US", options)
 
   return (
-    <h1>{playerInfo.name}</h1>
+    <div className="player-info">
+      <div className="player-info-photo">
+        <img src="https://placehold.co/300"></img>
+      </div>
+      <div className="player-info-data">
+        <h1>{playerInfo.name}</h1>
+        <p><span className="information-title"><strong>Birthday:</strong></span> {birthday} </p>
+        {playerInfo.nickname && <p><span className="information-title"><strong>Nickname:</strong></span> {playerInfo.nickname} </p>}
+        <p>{playerInfo.bio.json.content[0].content[0].value}</p>
+      </div>
+
+    </div>
   )
 }
